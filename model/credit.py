f70c8876aa919e925dc3923ea43ac5123102eb0b
taejin1221 <taejin7824@gmail.com>
2024-11-16 16:50:02 +0900

    Feat: member table과 관계 형성

--- model/credit.py
+++ model/credit.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, ForeignKey
 
 from config.database import Base
 
@@ -6,6 +6,8 @@ from config.database import Base
 class Credit(Base):
     id = Column(BigInteger, primary_key=True)
     balance = Column(BigInteger, default=0, nullable=False)
+
+    member_id = Column(BigInteger, ForeignKey("member.id"))
 
     created_at = Column(String(255), nullable=False)
     modified_at = Column(String(255), nullable=False)
