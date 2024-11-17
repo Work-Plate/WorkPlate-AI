804d4dcc56a6b62ce946a1252012f8d2d9a604b3
taejin1221 <taejin7824@gmail.com>
2024-11-18 00:44:08 +0900

    Feat: WorkRepository 작성
    Related to: #4

---
+++ repository/work_repository.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+
+from model.enum.physical_status import PhysicalStatus
+from model.work import Work
+
+
+class WorkRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def find_by_physical_status_and_location(self, physical_status: PhysicalStatus, location: str):
+        location_prefix = " ".join(location.split()[:2])
+        return (
+            self.session.query(Work)
+            .filter(Work.physical_status == physical_status,
+                    Work.location.like(f"{location_prefix}%")
+            )
+            .all()
+        )
