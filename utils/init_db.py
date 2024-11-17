9b1b18894ef58ade02b90e236c8b3d4c393d23cf
taejin1221 <taejin7824@gmail.com>
2024-11-18 00:59:08 +0900

    Feat: DB Init 파일 추가
    Related to: #4

---
+++ utils/init_db.py
@@ -0,0 +1,16 @@
+from config.database import engine
+from model.chatbot_message import ChatbotMessage
+from model.credit import Credit
+from model.member import Member
+from model.member_detail import MemberDetail
+from model.work_member import WorkMember
+from model.work import Work
+
+
+def create_tables():
+    Member.metadata.create_all(bind=engine)
+    MemberDetail.metadata.create_all(bind=engine)
+    WorkMember.metadata.create_all(bind=engine)
+    Work.metadata.create_all(bind=engine)
+    ChatbotMessage.metadata.create_all(bind=engine)
+    Credit.metadata.create_all(bind=engine)
