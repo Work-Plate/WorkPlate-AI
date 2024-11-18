debf874dca188636dbe122e54cff70d4cc2d629f
Jieun Lim <90019985+jieun-lim@users.noreply.github.com>
2024-11-18 16:57:34 +0900

    ✨ Feat: 챗봇 대화 기능 추가  (#9)
    * Feat: 챗봇 prompt 변경
    
    * Feat: 챗봇 prompt 변경 #8
    
    * Feat: 줄바꿈 추가 #8
    
    * Feat: 히스토리 활용 챗봇 프롬프트 삭제 #8

--- prompt/chat_prompts.py
+++ prompt/chat_prompts.py
@@ -1,6 +1,59 @@
 CHAT_WITH_BOT_TEMPLATE = (
-    "You are an AI chatbot interacting with a user. Your goal is to provide helpful, relevant, and thoughtful responses based on the context of previous messages and the current user's input. The conversation history and the user's latest message are provided. Respond clearly and naturally, staying consistent with the conversation's context. Be informative and engaging.\n\n"
-    "### Conversation History: "
-    "{previous_messages}\n\n"
-    "### User's Message:"
+    "You are an AI chatbot interacting with a user. Your goal is to provide helpful, relevant, and thoughtful responses to current user's input. Be informative and engaging.\n\n"
 )
+
+USER_INTENTION_CLASSIFY_TEMPLATE = (
+    """ 
+    You are an AI assistant that classifies user inputs into predefined categories. The categories and their descriptions are as follows:
+
+    1. **Work_Recommendation**: When the user asks for recommendations on small tasks (suited to their condition or preferences).
+    2. **Credit_Inquiry**: When the user requests to check the balance of "Yeopjeon" (credits or points).
+    3. **General_Conversation**: When the input does not fit into the above categories.
+
+    Classify the user's input into one of these categories. Respond with the category name only.
+
+### Examples: 
+    **Category: Work_Recommendation**
+    User Input: "내 소일거리 추천해줘"
+    Output: "Work_Recommendation"
+
+    User Input: "일거리 추천해줘"
+    Output: "Work_Recommendation"
+
+    User Input: "다리가 아픈데 일거리 추천해줘"
+    Output: "Work_Recommendation"
+
+    User Input: "사무직 관련 일거리 추천해줘"
+    Output: "Work_Recommendation"
+
+    **Category: Credit_Inquiry**
+    User Input: "엽전 남은 금액 조회해줘"
+    Output: "Credit_Inquiry"
+
+    User Input: "남은 엽전 조회해줘"
+    Output: "Credit_Inquiry"
+
+    User Input: "남은 엽전 얼마야?"
+    Output: "Credit_Inquiry"
+
+    User Input: "남은 엽전 크레딧 조회"
+    Output: "Credit_Inquiry"
+
+    User Input: "엽전 크레딧 조회"
+    Output: "Credit_Inquiry"
+
+    **Category: General_Conversation**
+    User Input: "안녕?"
+    Output: "General_Conversation"
+
+    User Input: "안녕하세요"
+    Output: "General_Conversation"
+
+    User Input: "오늘 심심해"
+    Output: "General_Conversation"
+
+    User Input: "오늘 날씨 어때?"
+    Output: "General_Conversation"
+    """
+)
+
--- service/chat_service.py
+++ service/chat_service.py
@@ -1,23 +1,35 @@
 from ai_model.gpt_model import GPTModel
-from entity.chatbot_message import Message
-from prompt.chat_prompts import CHAT_WITH_BOT_TEMPLATE
+from prompt.chat_prompts import CHAT_WITH_BOT_TEMPLATE, USER_INTENTION_CLASSIFY_TEMPLATE
 
 
 class ChatService:
     def __init__(self, gpt_model: GPTModel):
         self.gpt_model = gpt_model
 
-    def send_message(self, message: str, prev_messages: list[Message]):
+    def send_message(self, message: str):
         """
         사용자 메시지와 이전 대화 내용이 주어지면 챗봇의 응답을 생성합니다.
         :param message: 이번에 보낼 사용자의 메시지
-        :param prev_messages: 이전에 챗봇과 했던 대화내용
         :return: 챗봇의 응답
         """
-        prev_messages.sort(key=lambda x: x.createdAt)
-        prev_message = '\n'.join([msg.msg for msg in prev_messages])
+        # 사용자 message에 대한 텍스트 기반 응답 생성
+        try:
+            prompt = CHAT_WITH_BOT_TEMPLATE
+            response_text = self.gpt_model.generate_response(prompt, message)
+            return response_text
+        except Exception as e:
+            raise Exception(f"OpenAI API -> 텍스트 응답 생성 중 오류 발생: {str(e)}")
 
-        prompt = CHAT_WITH_BOT_TEMPLATE.format(previous_messages=prev_message)
-        response = self.gpt_model.generate_response(prompt, message)
-
-        return response
+    def inference_user_intention(self, user_message: str):  # 3가지 타입으로 사용자 의도 분류(엽전 조회, 일거리 추천, 일반 대화)
+        user_intention = self.gpt_model.generate_response(USER_INTENTION_CLASSIFY_TEMPLATE,
+                                                          user_message)  # 사용자 대화 의도
+        # user_intention:   # Work_Recommendation, Credit_Inquiry, General_Conversation
+        if user_intention == "Work_Recommendation":
+            # 작성
+            return  # task_type, text, additional_data
+        elif user_intention == "Credit_Inquiry":
+            # 함수 실행
+            return # task_type, text,
+        else:
+            response_text = self.send_message(user_message)
+            return response_text
