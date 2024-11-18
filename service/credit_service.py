77a5951a3f8d003c5d6de6635df8683c176a72cc
Jieun Lim <90019985+jieun-lim@users.noreply.github.com>
2024-11-18 17:08:01 +0900

    Feat: credit_service.py 추가 (#7)

---
+++ service/credit_service.py
@@ -0,0 +1,10 @@
+from repository.credit_repository import CreditRepository
+
+## 사용자 ID(member_id)를 입력받아, 엽전 정보 조회하는 서비스
+
+class CreditService:
+    def __init__(self, member_id:int):
+        self.member_id = member_id
+    def get_credit_balance(self): #  엽전 크레딧 잔액을 조회하는 서비스
+        credit = CreditRepository()
+        return credit.find_by_member_id(self.member_id).balance
