66303aec46c708cf9cc9915bef5bdc788e73be66
Jieun Lim <90019985+jieun-lim@users.noreply.github.com>
2024-11-18 17:13:12 +0900

    Feat: credit_service에 맞게 chat_service.py 수정 (#7)

--- service/chat_service.py
+++ service/chat_service.py
@@ -1,7 +1,8 @@
 from ai_model.gpt_model import GPTModel
 from prompt.chat_prompts import CHAT_WITH_BOT_TEMPLATE, USER_INTENTION_CLASSIFY_TEMPLATE
-from credit_service import CreditService # 추가
-from work_service import WorkService # 추가
+from credit_service import CreditService
+from work_service import WorkService
+
 
 class ChatService:
     def __init__(self, gpt_model: GPTModel):
@@ -22,15 +23,18 @@ class ChatService:
             raise Exception(f"OpenAI API -> 텍스트 응답 생성 중 오류 발생: {str(e)}")
 
     def inference_user_intention(self, user_message: str):  # 3가지 타입으로 사용자 의도 분류(엽전 조회, 일거리 추천, 일반 대화)
+        response_text = ''
         user_intention = self.gpt_model.generate_response(USER_INTENTION_CLASSIFY_TEMPLATE,
                                                           user_message)  # 사용자 대화 의도
-        # user_intention:   # Work_Recommendation, Credit_Inquiry, General_Conversation
+        # user_intention: Work_Recommendation, Credit_Inquiry, General_Conversation
         if user_intention == "Work_Recommendation":
-            # 작성
-            return  # task_type, text, additional_data
+            # 반환 받는 코드 작성 해야함
+            return
         elif user_intention == "Credit_Inquiry":
-            # 함수 실행
-            return # task_type, text,
+            credit_service = CreditService(self.member_id)
+            balance = credit_service.get_credit_balance()
+            response_text = f"잔여 엽전을 조회해드릴게요.\n 잔여 엽전 크레딧은 {balance}입니다. "
         else:
             response_text = self.send_message(user_message)
-            return response_text
+
+        return response_text
