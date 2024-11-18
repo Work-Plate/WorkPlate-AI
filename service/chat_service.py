44fb1cc38af13836629ba89dc241ca47b5ed1739
taejin1221 <taejin7824@gmail.com>
2024-11-18 20:02:05 +0900

    Docs: ChatService에 DocsString 추가 및 수정
    Related to: #7

--- service/chat_service.py
+++ service/chat_service.py
@@ -1,5 +1,6 @@
 from ai_model.gpt_model import GPTModel
 from credit_service import CreditService
+from entity.response_dto import ChatResponse
 from prompt.chat_prompts import CHAT_WITH_BOT_TEMPLATE, USER_INTENTION_CLASSIFY_TEMPLATE
 from work_service import WorkService
 
@@ -12,11 +13,10 @@ class ChatService:
 
     def send_message(self, message: str):
         """
-        사용자 메시지와 이전 대화 내용이 주어지면 챗봇의 응답을 생성합니다.
-        :param message: 이번에 보낼 사용자의 메시지
+        사용자 message에 대한 텍스트 기반 응답을 생성합니다.
+        :param message: 사용자의 메시지
         :return: 챗봇의 응답
         """
-        # 사용자 message에 대한 텍스트 기반 응답 생성
         try:
             prompt = CHAT_WITH_BOT_TEMPLATE
             response_text = self.gpt_model.generate_response(prompt, message)
@@ -24,7 +24,13 @@ class ChatService:
         except Exception as e:
             raise Exception(f"OpenAI API -> 텍스트 응답 생성 중 오류 발생: {str(e)}")
 
-    def inference_user_intention(self, member_id: int, user_message: str):  # 3가지 타입으로 사용자 의도 분류(엽전 조회, 일거리 추천, 일반 대화)
+    def inference_user_intention(self, member_id: int, user_message: str) -> ChatResponse:
+        """
+        3가지 타입으로 사용자 의도 분류(엽전 조회, 일거리 추천, 일반 대화)
+        :param member_id: 사용자의 ID
+        :param user_message: 사용자의 메시지
+        :return: 챗봇의 응답
+        """
         response_text = ''
         user_intention = self.gpt_model.generate_response(USER_INTENTION_CLASSIFY_TEMPLATE,
                                                           user_message)  # 사용자 대화 의도
