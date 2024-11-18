3d680a1315836dae99dab230983af9a97f999a35
Jieun Lim <90019985+jieun-lim@users.noreply.github.com>
2024-11-18 16:40:46 +0900

    Feat: 히스토리 활용 챗봇 프롬프트 삭제 #8

--- prompt/chat_prompts.py
+++ prompt/chat_prompts.py
@@ -2,13 +2,6 @@ CHAT_WITH_BOT_TEMPLATE = (
     "You are an AI chatbot interacting with a user. Your goal is to provide helpful, relevant, and thoughtful responses to current user's input. Be informative and engaging.\n\n"
 )
 
-CHAT_WITH_BOT_TEMPLATE_WITH_HISTORY = (
-    "You are an AI chatbot interacting with a user. Your goal is to provide helpful, relevant, and thoughtful responses based on the context of previous messages and the current user's input. The conversation history and the user's latest message are provided. Respond clearly and naturally, staying consistent with the conversation's context. Be informative and engaging.\n\n"
-    "### Conversation History: "
-    "{previous_messages}\n\n"
-    "### User's Message:"
-)
-
 USER_INTENTION_CLASSIFY_TEMPLATE = (
     """ 
     You are an AI assistant that classifies user inputs into predefined categories. The categories and their descriptions are as follows:
@@ -63,3 +56,4 @@ USER_INTENTION_CLASSIFY_TEMPLATE = (
     Output: "General_Conversation"
     """
 )
+
