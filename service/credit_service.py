7b44f1b67b2be049524d835d69cbda0f5fdbf28f
Jieun Lim <90019985+jieun-lim@users.noreply.github.com>
2024-11-18 17:53:00 +0900

    Feat: CreditService 클래스 내 member_id 인자 위치 변경 (#7)

--- service/credit_service.py
+++ service/credit_service.py
@@ -3,9 +3,9 @@ from repository.credit_repository import CreditRepository
 ## 사용자 ID(member_id)를 입력받아, 엽전 정보 조회하는 서비스
 
 class CreditService:
-    def __init__(self, member_id:int):
-        self.member_id = member_id
+    def __init__(self):
+        pass
 
-    def get_credit_balance(self): #  엽전 크레딧 잔액을 조회하는 서비스
+    def get_credit_balance(self, member_id:int): #  엽전 크레딧 잔액을 조회하는 서비스
         credit = CreditRepository()
-        return credit.find_by_member_id(self.member_id).balance
+        return credit.find_by_member_id(member_id).balance
