7494422549e86ad5014aa09b017e47e6b59745b1
taejin1221 <taejin7824@gmail.com>
2024-11-18 19:23:59 +0900

    Refactor: 주석을 Docstring으로 변경
    Related to: #7

--- service/credit_service.py
+++ service/credit_service.py
@@ -1,10 +1,14 @@
 from repository.credit_repository import CreditRepository
 
-## 사용자 ID(member_id)를 입력받아, 엽전 정보 조회하는 서비스
 
 class CreditService:
     def __init__(self, credit_repository: CreditRepository):
         self.credit_repository = credit_repository
 
-    def get_credit_balance(self, member_id:int): #  엽전 크레딧 잔액을 조회하는 서비스
+    def get_credit_balance(self, member_id: int) -> int:
+        """
+        사용자 ID(member_id)를 입력받아, 엽전 정보 조회하는 서비스
+        :param member_id: 사용자의 id
+        :return: 사용자의 엽전 정보
+        """
         return self.credit_repository.find_by_member_id(member_id).balance
