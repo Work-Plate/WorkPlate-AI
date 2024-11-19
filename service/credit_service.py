a0ade6f3be2564ab3d40a57d4555b52b982e971a
taejin1221 <taejin7824@gmail.com>
2024-11-19 17:06:29 +0900

    Refactor: username으로 정보 조회 불가 예외 발생
    Related to: #17

--- service/credit_service.py
+++ service/credit_service.py
@@ -11,4 +11,7 @@ class CreditService:
         :param username: 사용자의 이름
         :return: 사용자의 엽전 정보
         """
+        credit_info = self.credit_repository.find_by_username(username)
+        if not credit_info:
+            raise ValueError(f"주어진 {username}으로 엽전 정보를 찾을 수 없습니다.")
         return self.credit_repository.find_by_username(username).balance
