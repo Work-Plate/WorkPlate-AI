8f6ce919df1fa3185c12016c1ca0bb2eca1f9c01
taejin1221 <taejin7824@gmail.com>
2024-11-19 15:15:52 +0900

    Refactor: 반복적인 if 문 제거
    Related to: #14

--- service/work_service.py
+++ service/work_service.py
@@ -1,5 +1,6 @@
 from entity.work import WorkIdList
 from model.enum.physical_status import PhysicalStatus
+from model.enum.work_main_category import WorkMainCategory
 from model.enum.work_sub_category import WorkSubCategory
 from model.member_detail import MemberDetail
 from repository.member_detail_repository import MemberDetailRepository
@@ -10,6 +11,36 @@ class WorkService:
     def __init__(self, member_detail_repository: MemberDetailRepository, work_repository: WorkRepository):
         self.member_detail_repository = member_detail_repository
         self.work_repository = work_repository
+
+    @staticmethod
+    def _collect_recommendations(
+            candidate_work_list: list,
+            sub_preference: WorkSubCategory,
+            sub_experience: WorkSubCategory,
+            main_preference: WorkMainCategory,
+            main_experience: WorkMainCategory
+    ) -> list[int]:
+
+        work_list = []
+        criteria = [
+            (lambda w: w.sub_category == sub_preference),
+            (lambda w: w.sub_category == sub_experience),
+            (lambda w: w.main_category == main_preference),
+            (lambda w: w.main_category == main_experience),
+        ]
+
+        for condition in criteria:
+            if len(work_list) >= 10:
+                break
+
+            for work in candidate_work_list:
+                if work.id not in work_list and condition(work):
+                    work_list.append(work.id)
+
+                    if len(work_list) >= 10:
+                        break
+
+        return work_list
 
     def recommend(
             self,
@@ -35,24 +66,12 @@ class WorkService:
             )
         )
 
-        work_list = []
-        for work in candidate_work_list:
-            if work.sub_category == query_sub_preference:
-                work_list.append(work.id)
-
-        if len(work_list) <= 10:
-            for work in candidate_work_list:
-                if work.sub_category == query_sub_experience:
-                    work_list.append(work.id)
-
-        if len(work_list) <= 10:
-            for work in candidate_work_list:
-                if work.id not in work_list and work.main_category == member_detail.main_preference:
-                    work_list.append(work.id)
-
-        if len(work_list) <= 10:
-            for work in candidate_work_list:
-                if work.id not in work_list and work.main_category == member_detail.main_experience:
-                    work_list.append(work.id)
+        work_list = self._collect_recommendations(
+            candidate_work_list,
+            query_sub_preference,
+            query_sub_experience,
+            member_detail.main_preference,
+            member_detail.main_experience
+        )
 
         return WorkIdList(job_id_list=work_list)
